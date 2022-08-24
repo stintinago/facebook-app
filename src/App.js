59c35dfc446fd7309e1378d1f10f50cf2e32d6cd
Santiago Tintinago <tinti261999@gmail.com>
2022-08-24 08:51:40 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand, Form, FormGroup, Input,} from 'reactstrap';
+import fbLogo from './images/fb-nav-log.PNG'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="nav-facebook nav-shadow" color="faded" light>
+
+          <div className="d-flex flex-rowc mx-1">
+            <img
+              alt=''
+              src={fbLogo}
+              style={{ height: 40, width: 40 }}
+            />
+
+            <Form >
+              <FormGroup className=" mx-2">
+              <i class="fa fa-home"></i>
+                <Input style={{ height: '40px' }} className="facebook-search" type="text" name="search" id="search" placeholder="Buscar en Facebook" />
+              </FormGroup>
+            </Form>
+          </div>
+
+      </Navbar>
     </div>
   );
 }
